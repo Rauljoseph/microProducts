e229cd68f542af27f891e9c70efa73f49ff9a9ca
RaulMartinez <Rauljosephmartinez@gmail.com>
2024-12-24 00:17:29 -0300

    adding configurations for database and fix the pipeline

--- src/infrastructure/database/sequelizePG.ts
+++ src/infrastructure/database/sequelizePG.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ProductModel } from '../model/product';
 
 @Module({
   imports: [
@@ -14,10 +15,18 @@ import { SequelizeModule } from '@nestjs/sequelize';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
+        models: [ProductModel],
         autoLoadModels: true,
         synchronize: true,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
       }),
     }),
+    SequelizeModule.forFeature([ProductModel]),
   ],
 })
 export class DatabaseModule {}
